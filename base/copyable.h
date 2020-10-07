adb418bb47e6d4bf7d9443d33f35243b2adea9df
Ran Miller <ranmiller.leaf@gmail.com>
2020-10-07 21:34:31 +0800

    feat:add copyable class in copyable file

---
+++ base/copyable.h
@@ -0,0 +1,14 @@
+//
+// Created by zrzzzlll on 2020/10/7.
+//
+
+#ifndef MINI_MUDUO_BASE_COPYABLE_H_
+#define MINI_MUDUO_BASE_COPYABLE_H_
+namespace muduo{
+    class copyable{
+        protected:
+            copyable() = default;
+            ~copyable() = default;
+    };
+}
+#endif //MINI_MUDUO_BASE_COPYABLE_H_
